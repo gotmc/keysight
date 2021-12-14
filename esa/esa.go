580e72003c7c6d9fc45d0754d5a36501e16ab2ff
Matthew Rankin <matthew@questrail.com>
2021-12-13 21:38:28 -0600

    Parse frequency and trace data

--- esa/esa.go
+++ esa/esa.go
@@ -16,9 +16,9 @@ import (
 	"time"
 )
 
-type FreqUnits string
-type AmplitudeUnits string
+type FrequencyUnits string
 type TimeUnits string
+type AmplitudeUnits string
 
 type Trace struct {
 	Timestamp        time.Time
@@ -27,23 +27,27 @@ type Trace struct {
 	Model            string
 	SerialNum        string
 	CenterFreq       float64
-	CenterFreqUnits  FreqUnits
+	CenterFreqUnits  FrequencyUnits
 	Span             float64
-	SpanUnits        FreqUnits
+	SpanUnits        FrequencyUnits
 	RBW              float64
-	RBWUnits         FreqUnits
+	RBWUnits         FrequencyUnits
 	VBW              float64
-	VBWUnits         FreqUnits
+	VBWUnits         FrequencyUnits
 	RefLevel         float64
 	RefLevelUnits    AmplitudeUnits
 	SweepTime        float64
 	SweepTimeUnits   TimeUnits
 	NumPoints        int
-	FrequenciesUnits string
+	FreqLabel        string
+	Trace1Label      string
+	Trace2Label      string
+	Trace3Label      string
+	FreqUnits        string
 	Trace1Units      string
 	Trace2Units      string
 	Trace3Units      string
-	Frequencies      []float64
+	Frequency        []float64
 	Trace1           []float64
 	Trace2           []float64
 	Trace3           []float64
@@ -199,10 +203,66 @@ func ReadCSVFile(filename string) (Trace, error) {
 	scanner.Scan()
 	scanner.Scan()
 
+	// Parse 14th line, which should contain the labels for the frequency and
+	// trace data.
+	scanner.Scan()
+	line = scanner.Text()
+	s = strings.Split(line, ",")
+	if len(s) != 4 {
+		return trace, fmt.Errorf("error in trace label line: %s", line)
+	}
+	trace.FreqLabel = s[0]
+	trace.Trace1Label = s[1]
+	trace.Trace2Label = s[2]
+	trace.Trace3Label = s[3]
+
+	// Parse 15th line, which should contain the units for the frequency and
+	// trace data.
+	scanner.Scan()
+	line = scanner.Text()
+	s = strings.Split(line, ",")
+	if len(s) != 4 {
+		return trace, fmt.Errorf("error in trace units line: %s", line)
+	}
+	trace.FreqUnits = s[0]
+	trace.Trace1Units = strings.TrimSpace(s[1])
+	trace.Trace2Units = strings.TrimSpace(s[2])
+	trace.Trace3Units = strings.TrimSpace(s[3])
+
 	// Parse the remaining lines, which should now comply with RFC 4180 and be a
 	// standard CSV file.
+	trace.Frequency = make([]float64, trace.NumPoints)
+	trace.Trace1 = make([]float64, trace.NumPoints)
+	trace.Trace2 = make([]float64, trace.NumPoints)
+	trace.Trace3 = make([]float64, trace.NumPoints)
+	i = 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line = scanner.Text()
+		s = strings.Split(line, ",")
+		if len(s) != 4 {
+			return trace, fmt.Errorf("error in trace data line: %s", line)
+		}
+		freq, err := strconv.ParseFloat(strings.TrimSpace(s[0]), 64)
+		if err != nil {
+			return trace, fmt.Errorf("error parsing frequency %s for data point %d", s[0], i)
+		}
+		trace1, err := strconv.ParseFloat(strings.TrimSpace(s[1]), 64)
+		if err != nil {
+			return trace, fmt.Errorf("error parsing trace 1 %s for data point %d", s[1], i)
+		}
+		trace2, err := strconv.ParseFloat(strings.TrimSpace(s[2]), 64)
+		if err != nil {
+			return trace, fmt.Errorf("error parsing trace 2 %s for data point %d", s[2], i)
+		}
+		trace3, err := strconv.ParseFloat(strings.TrimSpace(s[3]), 64)
+		if err != nil {
+			return trace, fmt.Errorf("error parsing trace 3 %s for data point %d", s[3], i)
+		}
+		trace.Frequency[i] = freq
+		trace.Trace1[i] = trace1
+		trace.Trace2[i] = trace2
+		trace.Trace3[i] = trace3
+		i++
 	}
 
 	if err := scanner.Err(); err != nil {
--- esa/esa_test.go
+++ esa/esa_test.go
@@ -29,6 +29,15 @@ func TestReadCSVFile(t *testing.T) {
 				RefLevel:         106.99,
 				SweepTime:        0.085,
 				NumPoints:        401,
+				FreqLabel:        "",
+				Trace1Label:      "Trace 1",
+				Trace2Label:      "Trace 2",
+				Trace3Label:      "Trace 3",
+				FreqUnits:        "Hz",
+				Trace1Units:      "dBuV",
+				Trace2Units:      "dBuV",
+				Trace3Units:      "dBuV",
+				Trace1:           []float64{5.90097e+01, 5.92727e+01, 5.90557e+01},
 			},
 		},
 		{
@@ -45,6 +54,15 @@ func TestReadCSVFile(t *testing.T) {
 				RefLevel:         73.0103,
 				SweepTime:        0.0644205,
 				NumPoints:        401,
+				FreqLabel:        "",
+				Trace1Label:      "Trace 1",
+				Trace2Label:      "Trace 2",
+				Trace3Label:      "Trace 3",
+				FreqUnits:        "Hz",
+				Trace1Units:      "",
+				Trace2Units:      "",
+				Trace3Units:      "",
+				Trace1:           []float64{3.7123, 3.3353, 3.3773},
 			},
 		},
 	}
@@ -65,6 +83,22 @@ func TestReadCSVFile(t *testing.T) {
 			assertFloat64(t, "ref level", got.RefLevel, test.want.RefLevel, 0.0000001)
 			assertFloat64(t, "sweep time", got.SweepTime, test.want.SweepTime, 0.00000001)
 			assert(t, "num points", got.NumPoints, test.want.NumPoints)
+			assert(t, "freq label", got.FreqLabel, test.want.FreqLabel)
+			assert(t, "trace 1 label", got.Trace1Label, test.want.Trace1Label)
+			assert(t, "trace 2 label", got.Trace2Label, test.want.Trace2Label)
+			assert(t, "trace 3 label", got.Trace3Label, test.want.Trace3Label)
+			assert(t, "freq units", got.FreqUnits, test.want.FreqUnits)
+			assert(t, "trace 1 units", got.Trace1Units, test.want.Trace1Units)
+			assert(t, "trace 2 units", got.Trace2Units, test.want.Trace2Units)
+			assert(t, "trace 3 units", got.Trace3Units, test.want.Trace3Units)
+			assertFloat64(t, "t1[0]", got.Trace1[0], test.want.Trace1[0], 0.00000001)
+			assertFloat64(t, "t1[1]", got.Trace1[1], test.want.Trace1[1], 0.00000001)
+			// Check that the frequency and trace data have the correct number of
+			// data points.
+			assert(t, "freq len", len(got.Frequency), test.want.NumPoints)
+			assert(t, "trace 1 len", len(got.Trace1), test.want.NumPoints)
+			assert(t, "trace 2 len", len(got.Trace2), test.want.NumPoints)
+			assert(t, "trace 3 len", len(got.Trace3), test.want.NumPoints)
 		})
 	}
 }
